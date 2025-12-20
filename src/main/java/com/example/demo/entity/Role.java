6cfe3b990fff03b3a60945335d8b7cdc5b3efb63
bala-amypo <bala@amypo.in>
2025-12-20 16:12:42 +0000

    User commit

--- src/main/java/com/example/demo/entity/Role.java
+++ src/main/java/com/example/demo/entity/Role.java
@@ -10,14 +10,11 @@ public class Role {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = false)
+    @Column(unique = true)
     private String name;
 
     public Role() {}
-
-    public Role(String name) {
-        this.name = name;
-    }
+    public Role(String name) { this.name = name; }
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
